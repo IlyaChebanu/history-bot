4df461727c65dad01eacfb9f6e1bebf17ed7464e
Ilie Cebanu <cebanu5665@gmail.com>
2017-05-28 13:17:51 +0100

    Simplified, fixed counter issues

--- tweet_bot.py
+++ tweet_bot.py
@@ -14,41 +14,25 @@ def initialize_client(consumer_key, consumer_secret, access_key, access_secret):
     return client
 
 class Bot:
-    def __init__(self, tweets=[], counter=0, filename=None, interval=10):
-        # If tweets list wasn't passed, open file and read everything but the first line
-        # first line contains the counter
-        if not tweets:
-            try:
-                with codecs.open(filename, 'r', 'utf-8') as f:
-                    lines = f.readlines()
-                    # For some reason a \ufeff sometimes appears in front of the number
-                    lines[0] = lines[0].replace('\ufeff', '') # get rid of it
-                    self.counter = int(lines[0]) # first line
-                    self.tweets = lines[1:] # rest of file
-                    self.filename = filename
-            except ValueError:
-                print("Failed to set the counter")
-                quit()
-            except IOError:
-                print("Error opening the file")
-                quit()
-        else: # if a list of tweets was supplied
-            self.tweets = tweets
-            self.counter = counter
+    def __init__(self, name="historyiguess", counter=0, filename=None, interval=10):
+        try:
+            with codecs.open(filename, 'r', 'utf-8') as f:
+                self.tweets = f.readlines()
+                self.filename = filename
+        except IOError:
+            print("Error opening the file")
+            quit()
+        self.counter = counter
         self.interval = interval
+        self.name = name
 
-    # may come in useful in later programs, hence static
-    @staticmethod
-    def oauth_req(client, url, post_body="", http_method="POST"):
-    	response, data = client.request(url, method=http_method, body=post_body)
-    	return response, data
 
     @staticmethod
     def tweet(client, msg):
         # encode message into URL format
         tweet_msg = oauth.urlencode({'status': msg})
         # send HTTP request
-        response, data = Bot.oauth_req(client, TWEET_URL, tweet_msg)
+        response, data = client.request(TWEET_URL, body=tweet_msg, method="POST")
 
         try: # Log the tweet to the console with the HTTP status
             print("Tweeting \"{}\", HTTP: {}".format(msg.encode("utf-8").strip(), response.status))
@@ -68,17 +52,20 @@ class Bot:
             if not minute % self.interval and not second:
                 for i in range(4): # Try to post 4 times in case an error occurs
                     client = initialize_client(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+                    try:
+                        # Get data containing number of tweets
+                        response, data = client.request("https://api.twitter.com/1.1/users/show.json?screen_name=" + self.name, method="GET")
+                        # Parse the data to set the counter
+                        parse = json.loads(data.decode('latin1'))
+                        self.counter = int(parse["statuses_count"])
+                    except:
+                        continue # Try again if parsing failed due to an error
+
                     # when the counter gets to the end, wrap around to the start using modulus
                     msg = self.tweets[self.counter % len(self.tweets)]
                     response, data = __class__.tweet(client, msg)
 
                     if response.status in [200, 403]: # If successfully sent, or duplicate tweet
-                        self.counter += 1
-
-                        if self.filename: # If reading from a file, increment file's counter
-                            with codecs.open(self.filename, "w", "utf-8") as f:
-                                tweets = "".join(self.tweets)
-                                f.write(str(self.counter) + "\r\n" + tweets)
                         break # No need to try post again, break out of the loop
                     else:
                         print(data)
